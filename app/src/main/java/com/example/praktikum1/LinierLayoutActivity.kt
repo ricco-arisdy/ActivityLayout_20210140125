6684b794227a2b133c3fa066d7de554c7120daa1
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2023-10-03 23:29:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum1/LinierLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinierLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.linier_layout)
+    }
+}
---
+++ app/src/main/java/com/example/praktikum1/TabelActivity.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TabelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tabel)
+    }
+}
